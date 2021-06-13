1510fb24f94afef7a01bf55a702833e5c47adecc
il20afar <antoinefarley@gmail.com>
2021-06-13 14:18:41 -0400

    added simple-import-sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,12 @@ module.exports = {
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier', 'unused-imports'],
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'unused-imports',
+    'simple-import-sort',
+  ],
   rules: {
     'prettier/prettier': 'error',
     '@typescript-eslint/indent': ['error', 2],
@@ -23,5 +28,7 @@ module.exports = {
         argsIgnorePattern: '^_',
       },
     ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 };
